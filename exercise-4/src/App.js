51a6b23e17f7d5a70ab61c1dd792a28567d6654c
Charles Burgess <consulting@cvburgess.com>
2019-10-14 16:53:01 -0400

    add ex 4

---
+++ exercise-4/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { usePosition } from "use-position"; // https://github.com/trekhleb/use-position
+import Weather from "simple-react-weather"; // https://github.com/lopogo59/simple-react-weather
+import "./App.css";
+
+// Get an API key here: https://openweathermap.org/appid
+const API_KEY = "";
+
+const App = () => {
+  const { latitude, longitude } = usePosition();
+  const hasLocationData = Boolean(latitude && longitude);
+
+  return (
+    <div className="container">
+      <div className="weather">
+        {/* <Weather unit="F" city="Tampa" appid={API_KEY} /> */}
+        {hasLocationData && (
+          <Weather unit="F" lat={latitude} lon={longitude} appid={API_KEY} />
+        )}
+        {!hasLocationData && <h1>Please allow location access</h1>}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ exercise-4/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "react-bulma-components/dist/react-bulma-components.min.css";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
